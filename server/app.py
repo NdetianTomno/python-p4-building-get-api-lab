d8926f895e38ddcda6d7467ef6e5a1a3454aab4e
Tomno <tomnoviv@gmail.com>
2023-06-29 12:53:03 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -20,19 +21,33 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    all_bakeries = Bakery.query.all()
+    serialized_bakeries = [bakery.serialize() for bakery in all_bakeries]
+    return jsonify(serialized_bakeries)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        serialized_bakery = bakery.serialize()
+        return jsonify(serialized_bakery)
+    else:
+        return make_response(jsonify({'error': 'Bakery not found'}), 404)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    serialized_baked_goods = [baked_good.serialize() for baked_good in baked_goods]
+    return jsonify(serialized_baked_goods)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if baked_good:
+        serialized_baked_good = baked_good.serialize()
+        return jsonify(serialized_baked_good)
+    else:
+        return make_response(jsonify({'error': 'No baked goods found'}), 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,20 +1,80 @@
+from flask import Flask, make_response, jsonify
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+app = Flask(__name__)
 
-db = SQLAlchemy(metadata=metadata)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    baked_goods = db.relationship('BakedGood', backref='bakery', lazy=True)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'baked_goods': [baked_good.serialize() for baked_good in self.baked_goods]
+        }
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'bakery_id': self.bakery_id
+        }
+
+@app.route('/')
+def index():
+    return '<h1>Bakery GET API</h1>'
+
+@app.route('/bakeries')
+def bakeries():
+    all_bakeries = Bakery.query.all()
+    serialized_bakeries = [bakery.serialize() for bakery in all_bakeries]
+    return jsonify(serialized_bakeries)
+
+@app.route('/bakeries/<int:id>')
+def bakery_by_id(id):
+    bakery = Bakery.query.get(id)
+    if bakery:
+        serialized_bakery = bakery.serialize()
+        return jsonify(serialized_bakery)
+    else:
+        return make_response(jsonify({'error': 'Bakery not found'}), 404)
+
+@app.route('/baked_goods/by_price')
+def baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    serialized_baked_goods = [baked_good.serialize() for baked_good in baked_goods]
+    return jsonify(serialized_baked_goods)
+
+@app.route('/baked_goods/most_expensive')
+def most_expensive_baked_good():
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if baked_good:
+        serialized_baked_good = baked_good.serialize()
+        return jsonify(serialized_baked_good)
+    else:
+        return make_response(jsonify({'error': 'No baked goods found'}), 404)
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
